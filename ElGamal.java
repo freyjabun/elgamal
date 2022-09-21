f1d8032ade9c779b8dc979f47ece2d75bee20805
freyjabun <freyjabun@gmail.com>
2022-09-21 13:48:11 +0200

    Assignment 1

--- ElGamal.java
+++ ElGamal.java
@@ -1,15 +1,33 @@
 
 public class ElGamal {
 
-    int g = 666; //shared base
-    int p = 6661; //shared prime
-    int bobPK = 2227;
-
-    int y = (int) Math.random() + (p-1) + 1;
-
-    int gy = (int) Math.pow(g,y);
-
     public static void main(String[] args) {
-        System.out.println("skrrt");
+
+        //Assignment 1
+            
+        int g = 666; //shared base
+        int p = 6661; //shared prime
+        int bobPK = 2227;
+
+        int sk = (int) (Math.random() * (p-2) + 1); //Desired max and min values are -2
+
+        int gy = g;
+        int gxy = bobPK;
+
+        for(int i = 0; i<sk; i++){
+            gy = gy * g % p;  
+            gxy = gxy * bobPK % p;
+        } //Compute gy & gxy
+
+        int c = gxy * 2000;
+
+        //Encrypted message
+        System.out.println("Encrypted Message: "+ c);
+
+        //Assignment 2
+        
+
+
+
     }
 }
